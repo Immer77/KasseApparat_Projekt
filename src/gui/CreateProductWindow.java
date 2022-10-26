7b27d0e6a0fd7cf188cef97d8fce7873a1335a61
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2022-10-26 14:19:10 +0200

    Duplicate commit

--- src/gui/CreateProductWindow.java
+++ src/gui/CreateProductWindow.java
@@ -1,4 +1,60 @@
 package gui;
 
-public class CreateProductWindow {
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class CreateProductWindow extends Stage {
+
+    public CreateProductWindow (String title, Stage owner) {
+        this.initOwner(owner);
+        this.initStyle(StageStyle.UTILITY);
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.setMinHeight(300);
+        this.setMinWidth(500);
+        this.setResizable(false);
+
+        this.setTitle(title);
+        GridPane pane = new GridPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        this.setScene(scene);
+    }
+
+    private TextField txfName = new TextField();
+    private TextArea txaDescription = new TextArea();
+
+    public void initContent(GridPane pane) {
+        // pane.setGridLinesVisible(true);
+        pane.setPadding(new Insets(20));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
+        Label lblName = new Label("Produktnavn:");
+        pane.add(lblName, 0,0);
+        Label lblDescription = new Label("Beskrivelse:");
+        pane.add(lblDescription, 0,1);
+
+        pane.add(txfName, 1,0, 2, 1);
+        pane.add(txaDescription, 1, 1, 2, 2);
+
+        Button btnOK = new Button("Ok");
+        pane.add(btnOK, 1, 3);
+
+
+        Button btnCancel = new Button("Cancel");
+        pane.add(btnCancel, 2, 3);
+
+
+
+    }
+
 }
