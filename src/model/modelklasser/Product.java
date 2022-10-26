6f26c547c953fb2c97a318895a0e22b548e001c9
krishbee <kristoffer.frank@hotmail.com>
2022-10-26 12:08:18 +0200

    Iteration 3 - Annotations added for Product and ProductCategory

--- src/model/modelklasser/Product.java
+++ src/model/modelklasser/Product.java
@@ -5,6 +5,13 @@ public class Product {
     private String name;
     private String description;
 
+
+    /**
+     * creates object of the Product class
+     *
+     * @param name name of the product
+     * @param description a short description of the product
+     */
     public Product(String name, String description) {
         this.name = name;
         this.description = description;
