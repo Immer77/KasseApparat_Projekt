c00b3bb207fab418ba30608854af51dce332a7e8
krishbee <kristoffer.frank@hotmail.com>
2022-10-26 13:40:20 +0200

    Iteration 3 - GUI changes for tabpane setup

--- src/gui/StartWindow.java
+++ src/gui/StartWindow.java
@@ -1,5 +1,38 @@
 package gui;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class StartWindow {
 
+    public void start(Stage stage) {
+        stage.setTitle("Konference App");
+        BorderPane pane = new BorderPane();
+        this.initContent(pane);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void initContent(BorderPane pane) {
+        TabPane tabPane = new TabPane();
+        this.initTabPane(tabPane);
+        pane.setCenter(tabPane);
+    }
+
+    private void initTabPane(TabPane tabPane) {
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        Tab tabProductOverview = new Tab("Product overview");
+        tabPane.getTabs().add(tabProductOverview);
+
+        KonferencePane konferencePane = new KonferencePane();
+        tabKonference.setContent(konferencePane);
+        tabKonference.setOnSelectionChanged(event -> konferencePane.updateControls());
+
+    }
 }
