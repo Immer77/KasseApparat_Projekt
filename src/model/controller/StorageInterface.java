0f1df1be78f41a127d86ba172b73a139e9a6f728
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2022-10-26 12:25:37 +0200

    IT3 - ProductController helt done

--- src/model/controller/StorageInterface.java
+++ src/model/controller/StorageInterface.java
@@ -1,11 +1,12 @@
 package model.controller;
 
 import model.modelklasser.ProductCategory;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public interface StorageInterface {
     List<ProductCategory> getProductCategories();
-
     void addProductCategory(ProductCategory productCategory);
 
 }
