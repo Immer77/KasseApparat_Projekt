53d4fbb9791575fa944258722f9c9fb2e7a5bd91
Magnus Mejlgaard <mkmejlgaard@hotmail.com>
2022-10-26 12:47:42 +0200

    Magnus har lavet gui

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -14,6 +14,10 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import model.modelklasser.Product;
+import model.modelklasser.ProductCategory;
+
+import java.util.ArrayList;
 
 public class Gui extends Application {
     @Override
@@ -29,30 +33,83 @@ public class Gui extends Application {
 
     // -------------------------------------------------------------------------
 
-    private final ListView<Produkt> lvwProdukter = new ListView<>();
-    private final ListView<OrdreKategori> lvwKategorier = new ListView<>();
-    private final ArrayList<Kategori> kategorier = new ArrayList<>();
+    private final ListView<Product> lvwProdukter = new ListView<>();
+    private final ListView<ProductCategory> lvwKategorier = new ListView<>();
+    private final ArrayList<Product> products = new ArrayList<>();
+    private final ArrayList<ProductCategory> productCategories = new ArrayList<>();
 
     private void initContent(GridPane pane) {
+
+//        this.initContent();
 
         // pane.setGridLinesVisible(true);
         pane.setPadding(new Insets(20));
         pane.setHgap(10);
         pane.setVgap(10);
 
-        Label lblNameKategori = new Label("Ordrekategori:");
+        Label lblNameKategori = new Label("Product Category:");
         pane.add(lblNameKategori, 0, 0);
 
-        Label lblNameProdukt = new Label("Produkt:");
+        Label lblNameProdukt = new Label("Product:");
         pane.add(lblNameProdukt, 1, 0);
 
-        pane.add(lvwKategorier, 1, 1);
+        pane.add(lvwKategorier, 0, 1);
         lvwKategorier.setPrefWidth(200);
         lvwKategorier.setPrefHeight(200);
-        lvwKategorier.getItems().setAll(persons);
+        lvwKategorier.getItems().setAll();
+
+        pane.add(lvwProdukter, 1, 1);
+        lvwProdukter.setPrefWidth(200);
+        lvwProdukter.setPrefHeight(200);
+        lvwProdukter.getItems().setAll();
+
+        ChangeListener<Product> listener = (ov, o, n) -> this.productsItemSelected();
+        lvwProdukter.getSelectionModel().selectedItemProperty().addListener(listener);
+
+        Button btnCreateProductCategory = new Button("Create category");
+        pane.add(btnCreateProductCategory, 0, 0);
+        btnCreateProductCategory.setOnAction(event -> this.createProductCategoryAction());
+
+        Button btnCreateProduct = new Button("Create product");
+        pane.add(btnCreateProduct, 1, 0);
+        btnCreateProduct.setOnAction(event -> this.createProductAction());
+
+        this.initProduct();
+    }
+
+    // -------------------------------------------------------------------------
+
+    // Nogle produkter jeg har oprettet for at se om mit listview virker
+    private void initProduct(){
+        productCategories.add(new ProductCategory("Candy", "For kids");
+        products.add(new Product("Gummibear", "Sweet candy"));
+        products.add(new Product("Liqurice", "Tart candy"));
+        products.add(new Product("Sourz", "Sour drink"));
 
     }
 
+    // -------------------------------------------------------------------------
 
-
+    private void productsItemSelected() {
+        Product selected = lvwProdukter.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            lvwProdukter.getSelectionModel().getSelectedItem().toString();
+        } else {
+            lvwProdukter.refresh();
+        }
     }
+
+    private void initproductCategories(){
+        productCategories.add(new ProductCategory("Candy", "Goodies for kids"));
+    }
+
+    // Button actions
+    private void createProductCategoryAction(){
+        //TODO
+    }
+
+    private void createProductAction(){
+        //TODO
+    }
+
+}
