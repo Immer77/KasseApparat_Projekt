c6f6a6758ff81157f1fd28ef60eb66a9251eb4b2
Immer77 <peter_immersen@hotmail.com>
2022-10-26 10:54:39 +0200

    Commiting packages

--- src/model/modelklasser/Product.java
+++ src/model/modelklasser/Product.java
@@ -1,4 +1,28 @@
 package model.modelklasser;
 
 public class Product {
+
+    private String name;
+    private String description;
+
+    public Product(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/model/modelklasser/ProductCategory.java
+++ src/model/modelklasser/ProductCategory.java
@@ -1,4 +1,40 @@
 package model.modelklasser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductCategory {
+    private String title;
+    private String description;
+    private List<Product> products = new ArrayList<>();
+
+    public ProductCategory(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Product createProduct(String name, String description){
+        Product product = new Product(name, description);
+        products.add(product);
+        return product;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Product> getProducts() {
+        return new ArrayList<>(products);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
