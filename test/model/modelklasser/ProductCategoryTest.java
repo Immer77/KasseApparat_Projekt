7f2181ee734a6a901e4dcc11d015feaedac4c592
krishbee <kristoffer.frank@hotmail.com>
2022-10-26 11:34:40 +0200

    tc1_createProduct done

--- test/model/modelklasser/ProductCategoryTest.java
+++ test/model/modelklasser/ProductCategoryTest.java
@@ -1,13 +1,29 @@
 package model.modelklasser;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductCategoryTest {
+    ProductCategory productCategory;
+
+    @BeforeEach
+    void setUp() {
+        productCategory = new ProductCategory("Øl","Bajselademadder");
+    }
 
     @Test
-    void createProduct() {
+    void TC1_createProduct() {
+        // Arrange
+        String name = "Extra Pilsner";
+        String description = "5,1% alc. En pilsner der er lidt mere pilsner";
 
+        // Act
+        Product productTest = productCategory.createProduct(name,description);
+
+        // Assert
+        assertTrue(productCategory.getProducts().contains(productTest));
     }
 }
