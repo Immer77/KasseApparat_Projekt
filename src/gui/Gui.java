fe89d7f995d7b775fb0275e91228f498b8241777
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2022-10-26 13:32:47 +0200

    code documentation for GUI

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -37,53 +37,64 @@ public class Gui extends Application {
 
     private void initContent(GridPane pane) {
 
-//        this.initContent();
-
         // pane.setGridLinesVisible(true);
         pane.setPadding(new Insets(20));
         pane.setHgap(10);
         pane.setVgap(10);
 
+        //Categoy Label
         Label lblNameKategori = new Label("Produktkategorier:");
         pane.add(lblNameKategori, 0, 0);
 
+        //Product Label
         Label lblNameProdukt = new Label("Produkt:");
         pane.add(lblNameProdukt, 1, 0);
 
+        //List View of categories
         pane.add(lvwCategories, 0, 1);
         lvwCategories.setPrefWidth(200);
         lvwCategories.setPrefHeight(200);
         lvwCategories.getItems().setAll();
 
+        //List View of products
         pane.add(lvwProducts, 1, 1);
         lvwProducts.setPrefWidth(200);
         lvwProducts.setPrefHeight(200);
         lvwProducts.getItems().setAll();
 
+        //Listener for category list
         ChangeListener<ProductCategory> listener = (ov, o, n) -> this.productCategoryItemSelected();
         lvwCategories.getSelectionModel().selectedItemProperty().addListener(listener);
 
+        //create category button
         Button btnCreateProductCategory = new Button("Create category");
         pane.add(btnCreateProductCategory, 0, 0);
         btnCreateProductCategory.setOnAction(event -> this.createProductCategoryAction());
 
+        //create product button
         Button btnCreateProduct = new Button("Create product");
         pane.add(btnCreateProduct, 1, 0);
         btnCreateProduct.setOnAction(event -> this.createProductAction());
 
+        //initial methods
         this.initProduct();
         lvwCategories.getItems().setAll(productController.getProductCategories());
     }
 
     // -------------------------------------------------------------------------
 
-    // Nogle produkter jeg har oprettet for at se om mit listview virker
+    /**
+     * Creates initial products and categories
+     */
     private void initProduct(){
         productController.initContent();
     }
 
     // -------------------------------------------------------------------------
 
+    /**
+     * Called when listener detects changes in selection from category list. Updates product list to show the products contained in the category.
+     */
     private void productCategoryItemSelected() {
         ProductCategory selected = lvwCategories.getSelectionModel().getSelectedItem();
         if (selected != null) {
@@ -96,10 +107,17 @@ public class Gui extends Application {
 
 
     // Button actions
+
+    /**
+     * Opens a new window to create a new category of products
+     */
     private void createProductCategoryAction(){
         //TODO
     }
 
+    /**
+     * Opens a new window to create a new product, for the currently selected category
+     */
     private void createProductAction(){
         //TODO
     }
