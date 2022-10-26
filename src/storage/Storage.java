beda5266b4286bcdcfab4b1db5c113cb5beb37a1
Immer77 <peter_immersen@hotmail.com>
2022-10-26 13:02:42 +0200

    Iteration 3 - StorageInterface, Storage added singleton pattern and Comments

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -10,10 +10,15 @@ public class Storage implements StorageInterface {
     private static Storage unique_Storage;
     private List<ProductCategory> productCategories;
 
+    // Constructor
     private Storage(){
         productCategories = new ArrayList<>();
     }
 
+    /**
+     * We have here used the singletonPattern, so as we do not have different storages
+     * @return Returns storage if storage != null else it creates a new storage and returns it.
+     */
     public static Storage getUnique_Storage() {
         if(unique_Storage == null){
             unique_Storage = new Storage();
@@ -21,11 +26,19 @@ public class Storage implements StorageInterface {
         return unique_Storage;
     }
 
+    /**
+     * Returns a list of all the productcategories
+     * @return
+     */
     @Override
     public List<ProductCategory> getProductCategories() {
         return new ArrayList<>(productCategories);
     }
 
+    /**
+     * Adds a productcategory to the list of productcategories.
+     * @param productCategory
+     */
     @Override
     public void addProductCategory(ProductCategory productCategory) {
         if(!productCategories.contains(productCategory)){
