94f498e65c40a9a5ad85e7af67380a4c12913aa9
krishbee <kristoffer.frank@hotmail.com>
2022-11-01 13:37:03 +0100

    Iteration 5- Rental class added

---
+++ src/model/modelklasser/Rental.java
@@ -0,0 +1,63 @@
+package model.modelklasser;
+
+import java.time.LocalDate;
+
+public class Rental extends Order{
+
+    private String name;
+    private String description;
+    private LocalDate startDate;
+
+
+    public Rental(String name, String description, LocalDate startDate) {
+        super();
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+
+    @Override
+    public double calculateSumPriceForUnit(Unit unit){
+        double sumPrice = 0.0;
+        if(unit.equals(Unit.DKK)){
+            for (OrderLine orderLine : super.getOrderLines()){
+                if (orderLine.getPrice().getUnit().equals(unit)) {
+                    sumPrice += orderLine.calculateOrderLinePrice();
+                }
+            }
+        }
+        else{
+            throw new IllegalArgumentException("Can't calculate price for rental in punches");
+        }
+        return sumPrice;
+    }
+
+
+
+}
