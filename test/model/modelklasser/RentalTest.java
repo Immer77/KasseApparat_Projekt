407eb2e363ddbf02643c8c2a00016b276bab4c26
Magnus Mejlgaard <mkmejlgaard@hotmail.com>
2022-11-02 10:17:24 +0100

    Magnus skal commit for at kunne pull

---
+++ test/model/modelklasser/RentalTest.java
@@ -0,0 +1,49 @@
+package model.modelklasser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RentalTest {
+
+    Order order;
+    Price priceWithPant;
+
+
+    @BeforeEach
+    void setUp() {
+        order = new Order();
+        priceWithPant = mock(Price.class);
+    }
+
+    /**
+     * Udregner samlet deposit værdi for alle ordrelinjer i en ordre
+     */
+    @Test
+    void TC1_calculateDeposit() {
+        // Arrange
+
+        // Simulerede 3 forskellige priser
+        when(priceWithPant.getDepositPrice).thenReturn(1.0,1.5,3.0);
+        when(priceWithPant.getUnit()).thenReturn(Unit.DKK);
+
+        // Setting up our orderlines
+        OrderLine orderLine1 = order.createOrderLine(1,priceWithPant);
+        OrderLine orderLine2 = order.createOrderLine(1,priceWithPant);
+        OrderLine orderLine3 = order.createOrderLine(1,priceWithPant);
+
+        // Act
+        double result = order.calculateSumPriceForUnit(Unit.DKK);
+
+        //Assert
+        assertEquals(5.5,result);
+        assertTrue(order.getOrderLines().contains(orderLine1));
+        assertTrue(order.getOrderLines().contains(orderLine2));
+        assertTrue(order.getOrderLines().contains(orderLine3));
+
+    }
+}
