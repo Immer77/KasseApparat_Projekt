3df1fda1c46b3479e884b6294a443aae89e1210b
krishbee <kristoffer.frank@hotmail.com>
2022-11-02 10:46:15 +0100

    Iteration 5 - testrettelser

--- test/model/modelklasser/RentalTest.java
+++ test/model/modelklasser/RentalTest.java
@@ -3,21 +3,26 @@ package model.modelklasser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class RentalTest {
 
     Order order;
+    Rental rental;
     Price priceWithPant;
 
 
     @BeforeEach
     void setUp() {
+        String name = "Alt";
+        String description = "Vi lejer det hele";
+        LocalDate endDate = LocalDate.of(2022,12,1);
         order = new Order();
-        priceWithPant = mock(Price.class);
+        rental = new Rental(name,description,endDate);
     }
 
     /**
@@ -26,24 +31,29 @@ public class RentalTest {
     @Test
     void TC1_calculateDeposit() {
         // Arrange
+        Situation situation = mock(Situation.class);
+        Product product1 = new Product("Øl","noget øl");
+        Price pantPris1 = product1.createDeposit(1.0,Unit.DKK,situation);
+        Product product2 = new Product("Øl","noget øl");
+        Price pantPris2 = product2.createDeposit(1.5,Unit.DKK,situation);
+        Product product3 = new Product("Øl","noget øl");
+        Price pantPris3 = product3.createDeposit(3.0,Unit.DKK,situation);
 
-        // Simulerede 3 forskellige priser
-        when(priceWithPant.getProduct().getDepositPrice().getValue()).thenReturn(1.0,1.5,3.0);
-        when(priceWithPant.getUnit()).thenReturn(Unit.DKK);
 
         // Setting up our orderlines
-        OrderLine orderLine1 = order.createOrderLine(1,priceWithPant);
-        OrderLine orderLine2 = order.createOrderLine(1,priceWithPant);
-        OrderLine orderLine3 = order.createOrderLine(1,priceWithPant);
+        OrderLine orderLine1 = rental.createOrderLine(1,pantPris1);
+        OrderLine orderLine2 = rental.createOrderLine(1,pantPris2);
+        OrderLine orderLine3 = rental.createOrderLine(1,pantPris3);
+
 
         // Act
-        double result = order.calculateSumPriceForUnit(Unit.DKK);
+        double result = rental.calculateDeposit(Unit.DKK);
 
         //Assert
         assertEquals(5.5,result);
-        assertTrue(order.getOrderLines().contains(orderLine1));
-        assertTrue(order.getOrderLines().contains(orderLine2));
-        assertTrue(order.getOrderLines().contains(orderLine3));
+        assertTrue(rental.getOrderLines().contains(orderLine1));
+        assertTrue(rental.getOrderLines().contains(orderLine2));
+        assertTrue(rental.getOrderLines().contains(orderLine3));
 
     }
 }
