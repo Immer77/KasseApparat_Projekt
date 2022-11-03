4de183e3d627f042d60d0bf66d8d435a558808c5
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2022-11-03 13:26:26 +0100

    IT6 - Tour implemented

---
+++ src/model/modelklasser/Tour.java
@@ -0,0 +1,45 @@
+package model.modelklasser;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Tour extends Order{
+    //Fields ---------------------------------------------------------------------------------------------------------
+    private String name;
+    private String description;
+    private LocalTime time;
+
+    //Constructors ---------------------------------------------------------------------------------------------------
+    public Tour(LocalDate endDate, LocalTime Time) {
+        super();
+        this.setEndDate(endDate);
+        this.time = time;
+    }
+
+    //Methods - Get, Set & Add ---------------------------------------------------------------------------------------
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+
+    //Methods - Other ------------------------------------------------------------------------------------------------
+
+
+}
